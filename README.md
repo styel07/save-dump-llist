cf1cdd611246cbf223734080abbecab315597c0b
styel07 <eleazarcorpuz@gmail.com>
2015-09-11 10:39:01 -1000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Using a linked list, we are going to demonstrate a use case for a linked list and simulate a stack like you see in a stack trace (similar to what you see in your console when an error occurs), that replays the history of what we typed.
+
+Add a text box to an HTML page and add 2 buttons, save and dump.
+Every time the user clicks the first button to save, we are going to save the text input to our linked list as the most recent node or head.
+Whenever someone clicks the dump button we are going to dump out all of the input they've typed in to that point from most recent to oldest. This should be written as HTML to the page.
---
+++ linked-list.js
@@ -0,0 +1,140 @@
+var cl = console.log;
+
+var x = document.getElementById("txtArea").innerHTML;
+cl(x);
+alert(x);
+'use strict';
+
+var ll = function() {
+
+  var newLinkedList = {};
+  var head = null;
+  var tail = null;
+  var totalNode = 0;
+
+  var link = {
+    getHead : _getHead,
+    getTail : _getTail,
+    add : _add,
+    get : _get,
+    remove : _remove,
+    insert : _insert
+  };
+
+  function _getHead() {
+    return head;
+  }
+
+  function _getTail() {
+    return tail;
+  }
+
+  function _add(x) {
+    var newNode = {
+      value : value,
+      next : null
+    };
+
+    alert('hi'+ x);
+    if (_getTail() === null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
+    }
+
+    totalNode++;
+    alert('hi');
+    return tail;
+  }
+
+  function _get(number) {
+    if (number < -1 || number >= totalNode) {
+      return false;
+    } else {
+      if (number === 0) {
+        //theres a node connecting to it
+        return head;
+      }
+
+      //iterate through the linked list
+      var curNode = head.next;
+      for (var counter = 1; counter < number; counter++) {
+        // found it return that Node
+        if (counter === number) {
+          return curNode;
+        }
+        curNode = curNode.next;
+      }
+    }
+    return curNode;
+  }
+
+  function _remove(number) {
+    var pNode = _get(number - 1 );
+
+    var aNode = _get(number + 1);
+
+    //user tries to remove node outside linked list
+    if (number < 0 || number >= totalNode) {
+      return false;
+
+      //node to be removes is head
+    } else {
+
+      pNode.next = aNode;
+
+      totalNode--;
+      if ( number === 0) {
+        head = pNode.next;
+      }
+
+      //set the tail to the last node that is not false
+      if (_get(number) === tail) {
+        tail = pNode;
+      }
+    }
+  }
+
+  function _insert(value,number) {
+    // make a new node
+    var newNode = {
+      value : value,
+      next : null
+    };
+
+    // console.log('new node : ', newNode);
+    // Define current Node, previous Node
+    var currentNode = _get(number);
+    var previousNode = _get(number - 1);
+
+    // cl('this is new node',previousNode);
+    // if you want to insert a node to the beginning of the linkedList
+    if ( number === 0 ) {
+      // attach curNode newNode.next
+      newNode.next = currentNode;
+
+      // assign head to newNOde
+
+      head = newNode;
+    } else {
+      // attach new node.next to current node
+      newNode.next = currentNode;
+
+      // attach previous node.next to new node
+      previousNode.next = newNode;
+    }
+
+    // user tries to append node to end of linked list
+    // compare number to TotalNode (length)
+
+    cl('number', number);
+    cl('total node', totalNode);
+    if ( number < 0 || number >= totalNode ) {
+      return false;
+    }
+    totalNode++;
+  }
+  return link;
+};
